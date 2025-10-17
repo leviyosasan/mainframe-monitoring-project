24b9045bbe019d902880f1fc89ba9a5853a7475f
Emre Can Aslan <emrecan.aslan@vbt.com.tr>
2025-10-17 10:24:18 +0300

    all rmf tables srcs,asrm,trx

--- rmf_tables_emre.py
+++ rmf_tables_emre.py
@@ -71,7 +71,7 @@ def execute_query(query, params=None): # Executes the given query
 def get_token(): # Gets token
     global api_token, token_expiry_time
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    data = {"username": "VOBA", "password": "OZAN1238"}
+    data = {"username": "VOBA", "password": "OZAN1239"}
     try:
         response = requests.post(logon_url, headers=headers, data=data, verify=False)
         if response.status_code == 200:
